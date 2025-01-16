edbbd489d92f6f333e8a2ddda2eb90aa9ce3f4a3
Samant Patil <samantpatil.0425@gmail.com>
2025-01-16 11:59:23 +0000

    Commit 3

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
+cv = pickle.load(open("models/cv.pkl"))
+clf = pickle.load(open("models/clf.pkl"))
+
 
 @app.route("/")
 def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email = request.form.get('content')
+
     return render_template("index.html")
 
 if __name__ == "__main__":
